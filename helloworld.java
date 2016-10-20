108fc22d9bbc518b783834582ff1cba6ef6dd434
rahul <rahulrocky512@gmail.com>
2016-10-20 00:30:26 -0500

    gobisetty

--- helloworld.java
+++ helloworld.java
@@ -36,7 +36,7 @@ public class HelloWorld {
   public static void main(String[] args) {
 <<<<<<< HEAD
     System.out.println("Hello, World");
-    System.out.println("rahulrocky”);
+    System.out.println("rahulrocky gobisetty”);
 =======
   }
 }
