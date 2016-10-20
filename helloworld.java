8144ffc62274021ae29af8ad5893bf3023395254
rahul <rahulrocky512@gmail.com>
2016-10-20 01:14:07 -0500

    sai

--- helloworld.java
+++ helloworld.java
@@ -36,7 +36,7 @@ public class HelloWorld {
   public static void main(String[] args) {
 <<<<<<< HEAD
     System.out.println("Hello, World");
-    System.out.println("rahulrocky gobisetty”);
+    System.out.println("rahulrocky gobisetty sai rahul”);
 =======
   }
 }
